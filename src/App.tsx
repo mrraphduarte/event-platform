6b9ae7b9c3a39c04ecc737b034490a862e68b367
mrraphduarte <duarte.raphael@outlook.com>
2022-06-25 23:36:59 -0300

    inclusão de componentes e implementação de sideBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,9 @@
-import { gql, useQuery } from '@apollo/client'
-import { useEffect, useState } from 'react'
-import { client } from './lib/apollo'
-
-  const GET_LESSONS_QUERY = gql`
-    query {
-      lessons {
-        id
-        title
-      }
-    }
-  `
-
-  interface Lesson {
-    id: string;
-    title: string;
-  }
+import { Event } from "./pages/Event"
 
 function App() {
-  // FORMA OPCIONAL
-  // useEffect(() =>{
-  //   client.query({
-  //     query: GET_LESSONS_QUERY
-  //   }).then(response =>{
-  //     console.log(response.data)
-  //   })
-  // }, [])
-
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY)
-
+  
   return (
-    <ul>
-      {
-        data?.lessons.map(lesson =>{
-          return <li key={lesson.id}>
-            { lesson.title }
-          </li>
-        })
-      }    
-    </ul>
-
+    <Event />
   )
 }
 
